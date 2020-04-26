b170ec2dc12a3db0fa602e790ecb14d2b9f305e2
isidro lucena santiago <darksirius91@gmail.com>
2020-04-26 17:56:47 +0200

    frogete

---
+++ model.js
@@ -0,0 +1,80 @@
+Vue.component('aboutTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<h1></h1><h3></h3><p></p><div><img></img><img></img><img></img><img></img><img></img></div>'
+})
+Vue.component('expTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<h1></h1><listExp></listExp>'
+})
+Vue.component('eduTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<h1></h1><listEdu></listEdu>'
+})
+Vue.component('skillTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<h1></h1><h3></h3><div><img></img><img></div><h3></h3><ul><li></li></ul>'
+})
+Vue.component('interTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<h1></h1><p></p>'
+})
+Vue.component('awardTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<h1></h1><ul><li></li></ul>'
+})
+Vue.component('proyectTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<h3></h3><img></img>'
+})
+Vue.component('menuTab', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<img></img><a></a><a></a><a></a><a></a><a></a><a></a><a></a>'
+})
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
