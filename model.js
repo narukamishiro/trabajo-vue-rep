e6d682f273de9e0e1d221b6354f8932225a48033
isidro lucena santiago <darksirius91@gmail.com>
2020-04-26 21:32:14 +0200

    alfa no funcional

--- model.js
+++ model.js
@@ -1,50 +1,23 @@
 Vue.component('aboutTab', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<h1></h1><h3></h3><p></p><div><img></img><img></img><img></img><img></img><img></img></div>'
+  
+  template: '<h1>isidro lucena santiago</h1><h3>calle emilia pardo bazan,ed europan esc d 2,2 . granada . spain . 634-816-686 .<a>darksirius91@gmail.com</a></h3><p></p><div><img></img><img></img><img></img><img></img><img></img></div>'
 })
 Vue.component('expTab', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<h1></h1><listExp></listExp>'
+  template: '<h1>experience</h1><listExp></listExp>'
 })
 Vue.component('eduTab', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<h1></h1><listEdu></listEdu>'
+ 
+  template: '<h1>education</h1><listEdu></listEdu>'
 })
 Vue.component('skillTab', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<h1></h1><h3></h3><div><img></img><img></div><h3></h3><ul><li></li></ul>'
+
+  template: '<h1>skill</h1><h3></h3><div><img></img><img></div><h3></h3><ul><li></li></ul>'
 })
 Vue.component('interTab', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<h1></h1><p></p>'
+  template: '<h1>interests</h1><p></p>'
 })
 Vue.component('awardTab', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<h1></h1><ul><li></li></ul>'
+  template: '<h1>awards</h1><ul><li></li></ul>'
 })
 Vue.component('proyectTab', {
   data: function () {
@@ -54,27 +27,17 @@ Vue.component('proyectTab', {
   },
   template: '<h3></h3><img></img>'
 })
-Vue.component('menuTab', {
+Vue.component('listTab', {
   data: function () {
     return {
       count: 0
     }
   },
-  template: '<img></img><a></a><a></a><a></a><a></a><a></a><a></a><a></a>'
+  template: '<img></img><a>about</a><a>experience</a><a>education</a><a>skill</a><a>interests</a><a>awards</a><a>proyect</a>'
 })
-Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+Vue.component('listExp', {
+  template: '<h2></h2><h4></h4><h3></h3><p></p>'
 })
-Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+Vue.component('listEdu', {
+  template: '<h2></h2><h4></h4><h3></h3><p></p>'
 })
